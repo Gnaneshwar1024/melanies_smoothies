79e517c151435886b6bb82255142eb49f1e05bce
Gnaneshwar1024 <86462852+Gnaneshwar1024@users.noreply.github.com>
2025-06-05 15:27:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,6 +46,6 @@ if incredients_list:
 
             
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
+# smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+# # st.text(smoothiefroot_response.json())
+# sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
